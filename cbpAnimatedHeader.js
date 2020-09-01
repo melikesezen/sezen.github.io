ac3221aa3f3c2d51aee7cf51a42b0757fb565066
melikesezen <33052941+melikesezen@users.noreply.github.com>
2020-09-01 14:51:48 +0200

    Update cbpAnimatedHeader.js

--- cbpAnimatedHeader.js
+++ cbpAnimatedHeader.js
@@ -13,13 +13,13 @@ var cbpAnimatedHeader = (function() {
 	var docElem = document.documentElement,
 		header = document.querySelector( '.navbar-fixed-top' ),
 		didScroll = false,
-		changeHeaderOn = 250;
+		changeHeaderOn = 300;
 
 	function init() {
 		window.addEventListener( 'scroll', function( event ) {
 			if( !didScroll ) {
 				didScroll = true;
-				setTimeout( scrollPage, 200 );
+				setTimeout( scrollPage, 250 );
 			}
 		}, false );
 	}
